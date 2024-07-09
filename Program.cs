f289c1b08f82f57cf8bc142eb1cefbd13b75ff1b
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-07-09 17:48:40 -0300

    override e override toString

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace POOemCsharp
             pagamento.Vencimento = DateTime.Now;
             //OBJETO SEMPRE É UM TIPO DE REFERENCIA
 
+            Console.WriteLine(pagamento.Vencimento);
         }
     }
 
@@ -21,18 +22,34 @@ namespace POOemCsharp
         //propriedades = variaveis
         public DateTime Vencimento;
         //metodos = funções
-        public void Pagar(){}
+        public virtual void Pagar(){}
+
+        public override string ToString()
+        {
+            return Vencimento.ToString("dd/mm/yy");
+        }
     }
 
     class PagamentoBoleto : Pagamento
     {
         public string NumeroBoleto;
+
+        public override void Pagar()
+        {
+           //regra do boleto
+        }
+
     }
 
     
     class PagamentoCartao : Pagamento
     {
         public string NumeroCartao;
+
+        public override void Pagar()
+        {
+           //regra do cartão
+        }
     }
     
 }
