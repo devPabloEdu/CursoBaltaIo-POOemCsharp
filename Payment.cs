1d65a474fa0d5edefabf89aa257cdc6715f659c4
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-07-10 20:10:15 -0300

    delegade, metodos anonimos

--- Payment.cs
+++ Payment.cs
@@ -1,15 +1,101 @@
-namespace Payments
+using System;
+namespace POOemCsharp
 {
-    public partial class Payment
+     class Payment
     {
-        public class Pagamento{
-
-        }
-        public interface IPagamento
+        static void Main(string[] args)
         {
-             DateTime Vencimento { get; set; }
+            var personA = new Person(1, "Pablo Eduardo");
+            var personB = new Person(1, "Pablo Eduardo");
+            Console.WriteLine(personA.Equals(personB));
 
-             void Pagar(double valor);
+            // var pagamentoBoleto = new PagamentoBoleto();
+            // pagamentoBoleto.Pagar();
+            // pagamentoBoleto.Vencimento = DateTime.Now;
+            // pagamentoBoleto.NumeroBoleto = "123";
+            // var pagamento = new PagamentoBoleto();
+            // pagamento.Vencimento = DateTime.Now;
+            //OBJETO SEMPRE É UM TIPO DE REFERENCIA
+
+            // Console.WriteLine(pagamento.Vencimento);
         }
     }
+    public class Person : IEquatable<Person>
+    {
+        public Person(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+            
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public bool Equals(Person? other)
+        {
+            return Id == other.Id;
+            return Nome == other.Nome;
+        }
+    }
+    // public class Personal : Person
+    // {
+    //     public string CPF { get; set; }
+    // }
+
+    // public class Corporate : Person
+    // {
+    //     public string CNPJ { get; set; }
+    // }
+        //temos em c# os seguintes modificadores : private, protected, internal e public.
+    // public abstract class Pagamento
+    // {
+    //     public DateTime dataPagamento { get; set; }
+    //     public Pagamento()
+    //     {
+    //         Console.WriteLine("iniciando o pagamento.");
+    //         dataPagamento = DateTime.Now;
+            
+    //     }
+    //     public DateTime Vencimento { get; set; }
+
+    //     private DateTime _DataDePagamento;
+    //     public DateTime MyProperty
+    //     {
+    //         get { return _DataDePagamento; }
+    //         set { _DataDePagamento = value; }
+    //     }
+        
+    //     public virtual void Pagar(){}
+
+    //     public virtual void Pagar(string numero){}
+
+
+    //     public override string ToString()
+    //     {
+    //         return Vencimento.ToString("dd/mm/yy");
+    //     }
+    // }
+
+    //  class PagamentoBoleto : Pagamento
+    // {
+    //     public string NumeroBoleto;
+
+    //    public override void Pagar()
+    //     {
+    //        //regra do boleto
+    //     }
+
+    // }
+
+    
+    // class PagamentoCartao : Pagamento
+    // {
+    //     public string NumeroCartao;
+
+    //     public override void Pagar()
+    //     {
+    //        //regra do cartão
+    //     }
+    // }
+    
 }
--- Program.cs
+++ Program.cs
@@ -5,97 +5,11 @@ namespace POOemCsharp
     {
         static void Main(string[] args)
         {
-            var personA = new Person(1, "Pablo Eduardo");
-            var personB = new Person(1, "Pablo Eduardo");
-            Console.WriteLine(personA.Equals(personB));
-
-            // var pagamentoBoleto = new PagamentoBoleto();
-            // pagamentoBoleto.Pagar();
-            // pagamentoBoleto.Vencimento = DateTime.Now;
-            // pagamentoBoleto.NumeroBoleto = "123";
-            // var pagamento = new PagamentoBoleto();
-            // pagamento.Vencimento = DateTime.Now;
-            //OBJETO SEMPRE É UM TIPO DE REFERENCIA
-
-            // Console.WriteLine(pagamento.Vencimento);
+            //delegades => Anonymous Methods
         }
     }
-    public class Person : IEquatable<Person>
-    {
-        public Person(int id, string nome)
-        {
-            Id = id;
-            Nome = nome;
-            
-        }
-        public int Id { get; set; }
-        public string Nome { get; set; }
+    
+    public class Pagamento{
 
-        public bool Equals(Person? other)
-        {
-            return Id == other.Id;
-            return Nome == other.Nome;
-        }
     }
-    // public class Personal : Person
-    // {
-    //     public string CPF { get; set; }
-    // }
-
-    // public class Corporate : Person
-    // {
-    //     public string CNPJ { get; set; }
-    // }
-        //temos em c# os seguintes modificadores : private, protected, internal e public.
-    // public abstract class Pagamento
-    // {
-    //     public DateTime dataPagamento { get; set; }
-    //     public Pagamento()
-    //     {
-    //         Console.WriteLine("iniciando o pagamento.");
-    //         dataPagamento = DateTime.Now;
-            
-    //     }
-    //     public DateTime Vencimento { get; set; }
-
-    //     private DateTime _DataDePagamento;
-    //     public DateTime MyProperty
-    //     {
-    //         get { return _DataDePagamento; }
-    //         set { _DataDePagamento = value; }
-    //     }
-        
-    //     public virtual void Pagar(){}
-
-    //     public virtual void Pagar(string numero){}
-
-
-    //     public override string ToString()
-    //     {
-    //         return Vencimento.ToString("dd/mm/yy");
-    //     }
-    // }
-
-    //  class PagamentoBoleto : Pagamento
-    // {
-    //     public string NumeroBoleto;
-
-    //    public override void Pagar()
-    //     {
-    //        //regra do boleto
-    //     }
-
-    // }
-
-    
-    // class PagamentoCartao : Pagamento
-    // {
-    //     public string NumeroCartao;
-
-    //     public override void Pagar()
-    //     {
-    //        //regra do cartão
-    //     }
-    // }
-    
 }
